80280e8866e60d526de75f8f6b9aada29339e43e
Dogika <59515291+Dogika@users.noreply.github.com>
2024-05-25 11:31:09 -0400

    Create Obj.java

---
+++ Obj.java
@@ -0,0 +1,12 @@
+/**
+ * @brief An Object, with a shape (Sphere) and a Material
+ */
+public class Obj {
+    Sphere sphere;
+    Material material;
+    
+    public Obj(Sphere sphere, Material material) {
+        this.sphere = sphere;
+        this.material = material;
+    }
+}

5b6df59b29598506a3a8306012f06269fe0eb4ef
Dogika <59515291+Dogika@users.noreply.github.com>
2024-05-25 11:31:55 -0400

    Create Sphere.java

---
+++ Sphere.java
@@ -0,0 +1,12 @@
+/**
+ * A sphere, defined by its center and radius
+ */
+public class Sphere {
+    float3 Center;
+    float R;
+    
+    public Sphere(float3 Center, float R) {
+        this.Center = Center;
+        this.R = R;
+    }
+}
